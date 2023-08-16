0dac0ee7b54e8770a9751102072e472e193c4f63
Leslie Lamarche <leslie.lamarche@sri.com>
2023-08-16 11:41:31 -0700

    Revise Radar class to be more modular and testable.

--- amisrsynthdata/radar.py
+++ amisrsynthdata/radar.py
@@ -10,61 +10,111 @@ class Radar(object):
 
     def __init__(self, config):
 
-        self.read_config(config)
+        #self.read_config(config)
 
+        # These can be initalization parameters
+        self.radar_name = config['RADAR']['full_name']
+        self.radar_abbrev = config['RADAR']['abbreviation']
+        self.site_lat, self.site_lon, self.site_alt = config['RADAR']['site_coords']
+
+        beam_bc1 = config['RADAR'].get('beamcodes', [])
+        beam_az1, beam_el1, beam_ksys1 = self.beams_from_beam_codes(beam_bc1)
+
+        beam_az2 = config['RADAR'].get('beam_azimuth', [])
+        beam_el2 = config['RADAR'].get('beam_elevation', [])
+        beam_bc2, beam_ksys2 = self.beams_from_az_el(beam_az2, beam_el2)
+
+        # combine both beam arrays
+        self.beam_codes = np.concatenate((beam_bc1, beam_bc2))
+        self.beam_azimuth = np.concatenate((beam_az1, beam_az2))
+        self.beam_elevation = np.concatenate((beam_el1, beam_el2))
+        self.beam_ksys = np.concatenate((beam_ksys1, beam_ksys2))
+
+        # Save or calculate "on-the-fly"?
+        # add_beam function? - probably not
+
+        # Initializing Radar should basically set everything up
+        # Not much to calculate here that isn't based on standard radar characteristics
+        # Maybe time windows based on integration periods?
+
+        #az = self.beam_codes[:,1]
+        #el = self.beam_codes[:,2]
+
+        alt_bin_config = config['RADAR']['altitude_bins']
+        slant_range_config = config['RADAR']['acf_slant_range']
+
+        self.acf_slant_range, self.acf_lat, self.acf_lon, self.acf_alt = self.calculate_acf_gates(slant_range_config)
+        self.slant_range, self.lat, self.lon, self.alt = self.calculate_gates(alt_bin_config)
+
+        self.integration_period = config['RADAR']['integration_period']
+
+
+    def beams_from_beam_codes(self, beamcodes):
         # beams defined by standard beam code (beamcode files in package data)
         bc_file = files('amisrsynthdata.beamcodes').joinpath('bcotable_{}.txt'.format(self.radar_abbrev.lower().replace('-','')))
         bc_data = np.loadtxt(bc_file)
 
+        idx = np.where(np.in1d(bc_data[:,0], beamcodes))[0]
+        beam_azimuth = bc_data[idx,1]
+        beam_elevation = bc_data[idx,2]
+        beam_ksys = np.full(len(idx), np.nan)
 
-        idx = np.where(np.in1d(bc_data[:,0],self.beamcodes))[0]
-        beams_bc = np.array([bc_data[idx,0], bc_data[idx,1], bc_data[idx,2], np.full(len(idx), np.nan)]).T
+        return beam_azimuth, beam_elevation, beam_ksys 
 
-        # beams defined by azimuth and elevation
-        beams_ae = np.array([np.arange(len(self.beam_azimuth))+90000, self.beam_azimuth, self.beam_elevation, np.full(len(self.beam_azimuth), np.nan)]).T
+    def beams_from_az_el(self, beam_azimuth, beam_elevation):
+        beamcodes = np.arange(len(beam_azimuth))+90000
+        beam_ksys = np.full(len(beam_azimuth), np.nan)
 
-        # combine both beam arrays
-        self.beam_codes = np.concatenate((beams_bc, beams_ae), axis=0)
+        return beamcodes, beam_ksys
 
-        az = self.beam_codes[:,1]
-        el = self.beam_codes[:,2]
+       # # combine both beam arrays
+       # self.beam_codes = np.concatenate((beams_bc, beams_ae), axis=0)
 
-        # form list of altitude bins
-        altbins = list()
-        for segment in self.altitude_bins:
-            altbins.extend(np.arange(*segment))
+       # az = self.beam_codes[:,1]
+       # el = self.beam_codes[:,2]
+
+    def calculate_acf_gates(self, slant_range_config):
 
         # form slant range bins
-        self.slant_range_p = np.arange(*self.acf_slant_range)
-        self.lat_p, self.lon_p, self.alt_p = pm.aer2geodetic(az[:,None], el[:,None], self.slant_range_p[None,:], self.site_lat, self.site_lon, self.site_alt)
+        slant_range_p = np.arange(*slant_range_config)
+        lat_p, lon_p, alt_p = pm.aer2geodetic(self.beam_azimuth[:,None], self.beam_elevation[:,None], slant_range_p[None,:], self.site_lat, self.site_lon, self.site_alt)
+        return slant_range_p, lat_p, lon_p, alt_p
+
+    def calculate_gates(self, alt_bins_config):
+        # form list of altitude bins
+        altbins = list()
+        for segment in alt_bins_config:
+            altbins.extend(np.arange(*segment))
 
         # supress 'Mean of empty slice' warning so it doesn't clutter output - we expect empty slices at high altitudes
         with warnings.catch_warnings():
             warnings.filterwarnings(action='ignore', message='Mean of empty slice')
-            self.slant_range = np.array([[np.nanmean(np.where((beam>=altbins[i]) & (beam<altbins[i+1]), self.slant_range_p, np.nan)) for i in range(len(altbins)-1)] for beam in self.alt_p])
-        self.lat, self.lon, self.alt = pm.aer2geodetic(az[:,None], el[:,None], self.slant_range, self.site_lat, self.site_lon, self.site_alt)
+            slant_range = np.array([[np.nanmean(np.where((beam>=altbins[i]) & (beam<altbins[i+1]), self.acf_slant_range, np.nan)) for i in range(len(altbins)-1)] for beam in self.acf_alt])
+        lat, lon, alt = pm.aer2geodetic(self.beam_azimuth[:,None], self.beam_elevation[:,None], slant_range, self.site_lat, self.site_lon, self.site_alt)
+        return slant_range, lat, lon, alt
 
-        ke, kn, ku = pm.aer2enu(az, el, 1.0)
-        self.kvec = np.array([ke, kn, ku]).T
+        #ke, kn, ku = pm.aer2enu(az, el, 1.0)
+        #self.kvec = np.array([ke, kn, ku]).T  # Not needed
 
 
     def kvec_all_gates(self):
 
-        kx, ky, kz = pm.enu2uvw(self.kvec[:,0], self.kvec[:,1], self.kvec[:,2], self.site_lat, self.site_lon)
-        ke, kn, ku = pm.uvw2enu(kx[:,None], ky[:,None], kz[:,None], self.lat, self.lon)
-        kvec = np.array([ke, kn, ku]).transpose(1,2,0)
+        ke, kn, ku = pm.aer2enu(self.beam_azimuth, self.beam_elevation, 1.)
+        kx, ky, kz = pm.enu2uvw(ke, kn, ku, self.site_lat, self.site_lon)
+        ke_all, kn_all, ku_all = pm.uvw2enu(kx[:,None], ky[:,None], kz[:,None], self.lat, self.lon)
+        kvec = np.array([ke_all, kn_all, ku_all]).transpose(1,2,0)
 
         return kvec
 
 
-    def read_config(self, config):
-
-        self.radar_name = config['RADAR']['full_name']
-        self.radar_abbrev = config['RADAR']['abbreviation']
-        self.site_lat, self.site_lon, self.site_alt = config['RADAR']['site_coords']
-        self.beamcodes = config['RADAR'].get('beamcodes', [])
-        self.beam_azimuth = config['RADAR'].get('beam_azimuth', [])
-        self.beam_elevation = config['RADAR'].get('beam_elevation', [])
-        self.altitude_bins = config['RADAR']['altitude_bins']
-        self.acf_slant_range = config['RADAR']['acf_slant_range']
-        self.integration_period = config['RADAR']['integration_period']
+#    def read_config(self, config):
+#
+#        self.radar_name = config['RADAR']['full_name']
+#        self.radar_abbrev = config['RADAR']['abbreviation']
+#        self.site_lat, self.site_lon, self.site_alt = config['RADAR']['site_coords']
+#        self.beamcodes = config['RADAR'].get('beamcodes', [])
+#        self.beam_azimuth = config['RADAR'].get('beam_azimuth', [])
+#        self.beam_elevation = config['RADAR'].get('beam_elevation', [])
+#        self.altitude_bins = config['RADAR']['altitude_bins']
+#        self.acf_slant_range = config['RADAR']['acf_slant_range']
+#        self.integration_period = config['RADAR']['integration_period']
--- tests/test_Radar.py
+++ tests/test_Radar.py
@@ -38,43 +38,106 @@ def test_init(radar):
     assert radar.integration_period == 60.
 
 
-def test_beamcodes(radar, datafile):
-    bc_array = datafile['BeamCodes'][:]
-    assert np.array_equal(radar.beam_codes, bc_array, equal_nan=True)
+#def test_beamcodes(radar, datafile):
+#    bc_array = datafile['BeamCodes'][:]
+#    assert np.array_equal(radar.beam_codes, bc_array, equal_nan=True)
 
-def test_slant_range_p(radar, datafile):
-    sr_array = datafile['NeFromPower/Range'][:]
-    assert np.array_equal(radar.slant_range_p, sr_array)
+def test_beams_from_beam_codes(radar):
+    beamcodes = [64016, 64157, 64964, 65066]
+    truth_az = [  14.04, -154.3 ,  -34.69,   75.03]
+    truth_el = [90.  , 77.5 , 66.09, 65.56]
+    truth_ksys = [np.nan, np.nan, np.nan, np.nan]
+    azimuth, elevation, ksys = radar.beams_from_beam_codes(beamcodes)
+    assert np.array_equal(azimuth, truth_az)
+    assert np.array_equal(elevation, truth_el)
+    assert np.array_equal(ksys, truth_ksys, equal_nan=True)
 
-def test_slant_range_p_coords(radar):
+def test_beams_from_az_el(radar):
+    azimuth = [  14.04, -154.3 ,  -34.69,   75.03]
+    elevation = [90.  , 77.5 , 66.09, 65.56]
+    truth_bc = [90000, 90001, 90002, 90003]
+    truth_ksys = [np.nan, np.nan, np.nan, np.nan]
+    beamcodes, ksys = radar.beams_from_az_el(azimuth, elevation)
+    assert np.array_equal(beamcodes, truth_bc)
+    assert np.array_equal(ksys, truth_ksys, equal_nan=True)
+    
+def test_caculate_acf_gates(radar):
+    slant_range_config = [80000., 800000., 3000.]
+    truth_sr = np.arange(*slant_range_config)
+    truth_lat = list()
+    truth_lon = list()
+    truth_alt = list()
+    for az, el in zip(radar.beam_azimuth, radar.beam_elevation):
+        lat, lon, alt = pm.aer2geodetic(az, el, truth_sr, radar.site_lat, radar.site_lon, radar.site_alt)
+        truth_lat.append(lat)
+        truth_lon.append(lon)
+        truth_alt.append(alt)
 
-    for bc, lat_p, lon_p, alt_p in zip(radar.beam_codes, radar.lat_p, radar.lon_p, radar.alt_p):
-        az = bc[1]
-        el = bc[2]
-        lat, lon, alt = pm.aer2geodetic(az, el, radar.slant_range_p, radar.site_lat, radar.site_lon, radar.site_alt)
-        assert np.allclose(lat_p, lat)
-        assert np.allclose(lon_p, lon)
-        assert np.allclose(alt_p, alt)
+    slant_range, lat, lon, alt = radar.calculate_acf_gates(slant_range_config)
 
-def test_slant_range(radar, datafile):
-    sr_array = datafile['FittedParams/Range'][:]
-    assert np.allclose(radar.slant_range, sr_array, equal_nan=True)
+    assert np.array_equal(slant_range, truth_sr)
+    assert np.array_equal(lat, truth_lat)
+    assert np.array_equal(lon, truth_lon)
+    assert np.array_equal(alt, truth_alt)
 
-def test_slat_range_coords(radar, datafile):
-    lat_array = datafile['Geomag/Latitude'][:]
-    lon_array = datafile['Geomag/Longitude'][:]
-    alt_array = datafile['Geomag/Altitude'][:]
-    assert np.allclose(radar.lat, lat_array, equal_nan=True)
-    assert np.allclose(radar.lon, lon_array, equal_nan=True)
-    assert np.allclose(radar.alt, alt_array, equal_nan=True)
+def test_calculate_gates(radar):
+
+    altbins = np.arange(100., 800., 50.)*1000.
+    truth_sr = np.empty((len(radar.beam_codes), len(altbins)-1))
+    truth_lat = np.empty((len(radar.beam_codes), len(altbins)-1))
+    truth_lon = np.empty((len(radar.beam_codes), len(altbins)-1))
+    truth_alt = np.empty((len(radar.beam_codes), len(altbins)-1))
+    for b in range(len(radar.beam_codes)):
+        for i in range(len(altbins)-1):
+            abidx = np.argwhere((radar.acf_alt[b]>=altbins[i]) & (radar.acf_alt[b]<altbins[i+1])).flatten()
+            sr = np.mean(radar.acf_slant_range[abidx])
+            lat, lon, alt = pm.aer2geodetic(radar.beam_azimuth[b], radar.beam_elevation[b], sr, radar.site_lat, radar.site_lon, radar.site_alt)
+            truth_sr[b, i] = sr
+            truth_lat[b, i] = lat
+            truth_lon[b, i] = lon
+            truth_alt[b, i] = alt
+
+    slant_range, lat, lon, alt = radar.calculate_gates([[100000., 800000., 50000.]])
+
+    assert np.array_equal(slant_range, truth_sr, equal_nan=True)
+    assert np.array_equal(lat, truth_lat, equal_nan=True)
+    assert np.array_equal(lon, truth_lon, equal_nan=True)
+    assert np.array_equal(alt, truth_alt, equal_nan=True)
+
+#def test_slant_range_p(radar, datafile):
+#    sr_array = datafile['NeFromPower/Range'][:]
+#    assert np.array_equal(radar.acf_slant_range, sr_array)
+#
+#def test_slant_range_p_coords(radar):
+#
+#    for bc, lat_p, lon_p, alt_p in zip(radar.beam_codes, radar.lat_p, radar.lon_p, radar.alt_p):
+#        az = bc[1]
+#        el = bc[2]
+#        lat, lon, alt = pm.aer2geodetic(az, el, radar.slant_range_p, radar.site_lat, radar.site_lon, radar.site_alt)
+#        assert np.allclose(lat_p, lat)
+#        assert np.allclose(lon_p, lon)
+#        assert np.allclose(alt_p, alt)
+#
+#def test_slant_range(radar, datafile):
+#    sr_array = datafile['FittedParams/Range'][:]
+#    assert np.allclose(radar.slant_range, sr_array, equal_nan=True)
+#
+#def test_slat_range_coords(radar, datafile):
+#    lat_array = datafile['Geomag/Latitude'][:]
+#    lon_array = datafile['Geomag/Longitude'][:]
+#    alt_array = datafile['Geomag/Altitude'][:]
+#    assert np.allclose(radar.lat, lat_array, equal_nan=True)
+#    assert np.allclose(radar.lon, lon_array, equal_nan=True)
+#    assert np.allclose(radar.alt, alt_array, equal_nan=True)
 
 def test_kvec(radar):
 
     kvec = radar.kvec_all_gates()
 
-    for sr, bc, kv in zip(radar.slant_range, radar.beam_codes, kvec):
-        az = bc[1]
-        el = bc[2]
+    truth_kvec = list()
+    for sr, az, el in zip(radar.slant_range, radar.beam_azimuth, radar.beam_elevation):
+        #az = bc[1]
+        #el = bc[2]
         # aer -> uvw
         e, n, u = pm.aer2enu(az, el, 1.0)
         u, v, w = pm.enu2uvw(e, n, u, radar.site_lat, radar.site_lon, radar.site_alt)
@@ -82,6 +145,8 @@ def test_kvec(radar):
         lat, lon, alt = pm.aer2geodetic(az, el, sr, radar.site_lat, radar.site_lon, radar.site_alt)
         # uvw(new position) -> aer
         ke, kn, ku = pm.uvw2enu(u, v, w, lat, lon)
+        truth_kvec.append(np.array([ke, kn, ku]).T)
 
-        assert np.allclose(np.array([ke, kn, ku]).T, kv, equal_nan=True)
+
+    assert np.array_equal(kvec, truth_kvec, equal_nan=True)
 
